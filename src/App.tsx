236a2a30b914baaa56cfcd719b3a3927a5e56c34
Oskar <oskarkaklewski@gmail.com>
2024-06-24 16:14:20 +0200

    Created the initial component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { useState } from 'react';
+import words from './wordList.json';
+import { HangmanDrawing } from './HangmanDrawing';
+import { HangmanWord } from './HangmanWord';
+import { Keyboard } from './Keyboard';
+
 function App() {
-	return <h1>Hi</h1>;
+	const [wordToGuess, setWordToGuess] = useState(() => {
+		return words[Math.floor(Math.random() * words.length)];
+	});
+	const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+	const incorrectLetters = guessedLetters.filter(
+		(letter) => !wordToGuess.includes(letter)
+	);
+
+	return (
+		<div
+			style={{
+				maxWidth: '800px',
+				display: 'flex',
+				flexDirection: 'column',
+				gap: '2rem',
+				margin: '0 auto',
+				alignItems: 'center',
+			}}>
+			<div style={{ fontSize: '2rem', textAlign: 'center' }}>
+				Lose Win
+			</div>
+			<HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+			<HangmanWord />
+			<div style={{ alignSelf: 'stretch' }}>
+				<Keyboard />
+			</div>
+		</div>
+	);
 }
 
 export default App;
