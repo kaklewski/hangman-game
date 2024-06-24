20f6b30671132563efc46dc589203afdf73e7dfa
Oskar <oskarkaklewski@gmail.com>
2024-06-24 17:16:09 +0200

    Added the message to be displayed when winning or loosing

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,14 @@ function App() {
 		(letter) => !wordToGuess.includes(letter)
 	);
 
+	// Check if the number of incorrect letters is equal or greater than the number of the body parts
+	const isLoser = incorrectLetters.length >= 6;
+
+	// If every single letter is included in guessedLetters, it means that the player won
+	const isWinner = wordToGuess
+		.split('')
+		.every((letter) => guessedLetters.includes(letter));
+
 	const addGuessedLetter = useCallback(
 		(letter: string) => {
 			if (guessedLetters.includes(letter)) return;
@@ -51,7 +59,8 @@ function App() {
 				alignItems: 'center',
 			}}>
 			<div style={{ fontSize: '2rem', textAlign: 'center' }}>
-				Lose Win
+				{isWinner && 'Winner! - Refresh to try again'}
+				{isLoser && 'Nice try - Refresh to try again'}
 			</div>
 			<HangmanDrawing numberOfGuesses={incorrectLetters.length} />
 			<HangmanWord
@@ -60,6 +69,7 @@ function App() {
 			/>
 			<div style={{ alignSelf: 'stretch' }}>
 				<Keyboard
+        disabled={isWinner || isLoser}
 					activeLetters={guessedLetters.filter((letter) =>
 						wordToGuess.includes(letter)
 					)}
